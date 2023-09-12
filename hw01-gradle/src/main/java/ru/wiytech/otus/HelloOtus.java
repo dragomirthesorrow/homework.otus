c2ea9a35f3ea64beda40c7319f441a5302fe15b6
a.onishchenko <a.onishchenko@x5.ru>
2023-09-12 16:20:18 +0300

    Otus HW01-gradle

---
+++ hw01-gradle/src/main/java/ru/wiytech/otus/HelloOtus.java
@@ -0,0 +1,10 @@
+package ru.wiytech.otus;
+
+import com.google.common.math.BigIntegerMath;
+
+public class HelloOtus {
+
+    public static void main(String[] args) {
+        System.out.println(BigIntegerMath.factorial(10));
+    }
+}
